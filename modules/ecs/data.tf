3c75b9a6e3750159d8bf5e2013845b605016a2c1
unknown <praveen1664@outlook.com>
2022-08-18 12:26:45 +0530

    Added time lapse in efs creation & mounting

--- modules/ecs/data.tf
+++ modules/ecs/data.tf
@@ -1,2 +1,21 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role_policy" {
+  statement {
+    sid = "assume"
+    effect = "Allow"
+    principals {
+      identifiers = ["ecs-tasks.amazonaws.com"]
+      type = "Service"
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy" "AmazonECSTaskExecutionRolePolicy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+
